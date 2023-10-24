55ab62144243d9e7d7104a72982c035a0a1135f8
Gavin <G00402296@atu.ie>
2023-10-24 15:44:37 +0100

    Added the blocking approach

---
+++ src/main/java/ie/atu/week5blockingvsnon/Week5BlockingVsNonApplication.java
@@ -0,0 +1,18 @@
+package ie.atu.week5blockingvsnon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Week5BlockingVsNonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Week5BlockingVsNonApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){return new RestTemplate();}
+}
---
+++ src/test/java/ie/atu/week5blockingvsnon/Week5BlockingVsNonApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5blockingvsnon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5BlockingVsNonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
